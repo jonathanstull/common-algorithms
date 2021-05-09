b2b7baf75a4e1c80fb3b59a2272f6f2e7c485c8a
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-08 23:09:23 -0700

    completes binary search method

--- src/js/binary-search.js
+++ src/js/binary-search.js
@@ -1,19 +1,27 @@
 export default class Searches {
 
-  simpleSearch(listArray, item) {
-    for (let i = 0; i < listArray.length; i++) {
-      if (listArray[i] === item) {
+  simpleSearch(sortedArray, item) {
+    for (let i = 0; i < sortedArray.length; i++) {
+      if (sortedArray[i] === item) {
         return i;
       }
     }
   }
 
-  binarySearch(listArray, item) {
+  binarySearch(sortedArray, item) {
     let low = 0;
-    let high = listArray.length - 1;
+    let high = sortedArray.length - 1;
     while (low <= high) {
       let mid = (low + high) / 2;
-      
+      let guess = sortedArray[mid];
+      if (guess === item) {
+        return mid;
+      }
+      else if (guess > item) {
+        high = mid - 1;
+      }
+      else low = mid + 1;
     }
+    return null;
   }
 }
